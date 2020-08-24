ca39a06f869f23fff37ea9d5ffda1d84ec2abcc2
Leandro <gringoperalta@gmail.com>
2020-08-25 10:03:13 +1200

    Initial commit with basic UI.

---
+++ app/src/main/java/com/example/pintest/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.pintest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import androidx.core.widget.addTextChangedListener
+import com.example.pintest.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding;
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        this.binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(this.binding.root)
+
+        this.binding.input.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                binding.output.text = getString(R.string.your_pin_is, s);
+            }
+        })
+    }
+}
