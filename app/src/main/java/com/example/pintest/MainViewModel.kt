afee4762f2009e5080511a5b459f5a063cee1aa2
Leandro <gringoperalta@gmail.com>
2020-08-25 14:55:34 +1200

    Implement IPB, PAN Block and PIN Block algorithms.

--- app/src/main/java/com/example/pintest/MainViewModel.kt
+++ app/src/main/java/com/example/pintest/MainViewModel.kt
@@ -3,7 +3,10 @@ package com.example.pintest
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import java.lang.NumberFormatException
+import java.util.*
+import kotlin.experimental.xor
+import kotlin.math.min
+import kotlin.random.Random
 
 const val PAN = "1111222233334444"
 
@@ -23,4 +26,34 @@ class MainViewModel : ViewModel() {
     }
 }
 
-fun toPINBlock(pin: String, pan: String) = ""
+fun toIPB(pin: String): String {
+    val ipb = CharArray(16);
+    ipb[0] = '3' // ISO-3
+    ipb[1] = Integer.toHexString(pin.length)[0]
+    pin.toCharArray().copyInto(ipb, 2)
+    val options = arrayOf('a', 'b', 'c', 'd', 'f')
+    for (i in 2 + pin.length until ipb.size) {
+        ipb[i] = options[Random.nextInt(options.size)]
+    }
+    return String(ipb)
+}
+
+fun toPANBlock(pan: String): String {
+    val panBlock = CharArray(16)
+    for (i in 0..3) {
+        panBlock[i] = '0'
+    }
+    pan.takeLast(12).toCharArray().copyInto(panBlock, 4)
+    return String(panBlock)
+}
+
+fun toPINBlock(input: String, pan: String): String {
+    when {
+        input.toLongOrNull() == null -> throw Exception("Invalid PIN format")
+        input.length < 4 -> throw Exception("Invalid PIN length")
+    }
+
+    val ipb = toIPB(input);
+    val panBlock = toPANBlock(pan);
+    return (ipb.toLong(16) xor panBlock.toLong(16)).toString(16)
+}
---
+++ app/src/test/java/com/example/pintest/PINBlockUnitTest.kt
@@ -0,0 +1,50 @@
+package com.example.pintest
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class PINBlockUnitTest {
+
+    @Test
+    fun toIPB(){
+        // Arrange
+        val pin = "123456"
+
+        // Act
+        val ipb = com.example.pintest.toIPB(pin)
+
+        // Assert
+        assertTrue(ipb.startsWith("36123456"))
+    }
+
+    @Test
+    fun toPANBlock(){
+        // Arrange
+        val pan = "111222333444555";
+
+        // Act
+        val panBlock = toPANBlock(pan)
+
+        // Assert
+        assertEquals("0000222333444555", panBlock)
+    }
+
+    @Test
+    fun toPINBlock() {
+        // Arrange
+        val pin = "123456";
+        val pan = "111222333444555";
+
+        // Act
+        val pinBlock = toPINBlock(pin, pan)
+
+        // Assert
+        assertTrue(pinBlock.startsWith("36121675"))
+    }
+}
